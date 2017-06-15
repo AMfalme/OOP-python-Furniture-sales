d9bd85beed70f05ef5cb825f4b011cddfff4f061
AMfalme <mfalmegriffin@gmail.com>
2017-06-15 12:18:15 +0300

    changed class to electronics and computer inherits from electronics

--- OOP_file.py
+++ OOP_file.py
@@ -2,12 +2,49 @@ from abc import ABCMeta, abstractmethod
 
 
 
-class Furniture(object):
-	"""docstring for Furniture
-	This is an abstract Base class for a vendor selling furniture of various types.
+class Electronics(object):
+	"""docstring for Electronics
+	This is an abstract Base class for a vendor selling Electronics of various types.
 
 	"""
-	def __init__(self, arg):
-		super(Furniture, self).__init__()
-		self.arg = arg
+	__metaclass__ = ABCMeta
+	def __init__(self, make, year_of_manufacture, sale_date, purchase_price, selling_price, purchase_date):
+		super(Electronics, self).__init__()
+		self.make = make
+		self.year_of_manufacture = year_of_manufacture
+		self.sale_date = sale_date
+		self.purchase_price = purchase_price
+		self.selling_price
+	def profit_after_sale(self):
+		profit = self.selling_price - self.purchase_price
+		return profit
+
+	def make(self):
+		return self.make
+
+	def days_in_store(self):
+		days_in_store 
+	@abstractmethod
+	def type_of_electronic(self):
+		return self.make
+
+
+class computer(Electronics):
+	"""docstring for computer"""
+	def __init__(self, model):
+		self.model = model
+	def type_of_electronic(self):
+		return "Computer"
+	def insert_ram_capacity(self):
+		ram_in_gb = raw_input("What is the ram of this computer")
+		self.ram = ram_in_gb
+		return self.ram
+
+	def Insert_processor_capacity(self):
+		processor = raw_input("kindly input the processor capacity on this computer")
+laptop = computer("HP")
+
+print(laptop.make)
+		
+		
 		
